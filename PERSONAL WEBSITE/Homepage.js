73a084f913e93797d8391224fd0d330757824147
kuriapeter <126658519+kuriapeter@users.noreply.github.com>
2023-11-16 01:58:51 +0300

    Add files via upload

---
+++ PERSONAL WEBSITE/Homepage.js
@@ -0,0 +1,7 @@
+// Example: Add an alert on page load
+window.onload = function() {
+    alert("Welcome to my website!");
+  };
+  
+  // You can add more JavaScript for animations, form validation, etc.
+  
---
+++ PERSONAL WEBSITE/childhood.js
@@ -0,0 +1,7 @@
+// Example: Add an alert on page load
+window.onload = function() {
+    alert("Explore my childhood memories!");
+  };
+  
+  // You can add more JavaScript for animations, form validation, etc.
+  
